2f76556f331ebf58985bddaf1808cf36e4b7b439
ntoulemont <n.toulemont@gmail.com>
2022-04-16 15:44:20 +0200

    feat: grid traveler and fib

---
+++ scripts/dynamic_programming/fib_memo.ts
@@ -0,0 +1,15 @@
+function fib(num: number, memo = new Map()): any {
+  if (memo.get(num)) return memo.get(num)
+  if (num <= 2) return 1
+  memo.set(num, fib(num - 1, memo) + fib(num - 2, memo))
+  return memo.get(num)
+}
+
+export const fib_memo = {
+  fn: () => {
+    console.log(fib(6))
+    console.log(fib(7))
+    console.log(fib(8))
+    console.log(fib(50))
+  },
+}
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,1 +1,2 @@
-export * from './fib_memo'
+export * from './dynamic_programming/fib_memo'
+export * from './dynamic_programming/grid_traveler_memo'
