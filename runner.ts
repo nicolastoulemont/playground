f262dcfd0e63f5d1da39f2de014aae3d853bccf3
ntoulemont <n.toulemont@gmail.com>
2022-04-16 15:53:47 +0200

    feat: name changes

--- runner.ts
+++ runner.ts
@@ -1,4 +1,4 @@
-import * as scripts from './scripts'
+import * as scriptsRecord from './scripts'
 
 const args = process.argv.slice(2)
 
@@ -6,9 +6,9 @@ if (args.length === 0) throw new Error('No arguments provided')
 
 const [target] = args
 
-const fns = Object.entries(scripts).map(([key, value]) => ({ name: key, ...value }))
+const scripts = Object.entries(scriptsRecord).map(([key, value]) => ({ name: key, ...value }))
 
-const script = fns.find((fn) => fn.name === target)
+const script = scripts.find((fn) => fn.name === target)
 
 if (!script) throw new Error(`No script found for ${target}`)
 
