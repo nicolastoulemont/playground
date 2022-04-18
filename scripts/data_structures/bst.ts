4209e608bfdd33d553c910c2564b2c0ff47828e9
ntoulemont <n.toulemont@gmail.com>
2022-04-18 11:39:25 +0200

    added barrel import files into scripts sub dirs and started moving data structures to the playground

---
+++ scripts/data_structures/bst.ts
@@ -0,0 +1,200 @@
+class Node<T> {
+  value: T
+  left: Node<T> | null
+  right: Node<T> | null
+  constructor(value: T, left = null, right = null) {
+    this.value = value
+    this.left = left
+    this.right = right
+  }
+}
+
+class BST<T> {
+  root: Node<T> | null
+  constructor() {
+    this.root = null
+  }
+
+  searchTree(node: Node<T>, value: T): void | null {
+    // Iterate left
+    if (value < node.value) {
+      if (node.left === null) {
+        node.left = new Node(value)
+        return
+      }
+      return this.searchTree(node.left, value)
+    } else if (value > node.value) {
+      // Iterate Right
+      if (node.right === null) {
+        node.right = new Node(value)
+        return
+      }
+      return this.searchTree(node.right, value)
+    } else {
+      return null
+    }
+  }
+
+  inOrderTraverse(cb: (node: Node<T>) => void) {
+    this.inOrderTraverseNode(this.root, cb)
+  }
+
+  inOrderTraverseNode(node: Node<T> | null, cb: (node: Node<T>) => void) {
+    if (!node) return
+    this.inOrderTraverseNode(node.left, cb)
+    cb(node)
+    this.inOrderTraverseNode(node.right, cb)
+  }
+
+  add(value: T) {
+    if (this.root === null) {
+      this.root = new Node(value)
+      return
+    }
+    return this.searchTree(this.root as Node<T>, value)
+  }
+
+  getMin() {
+    let current = this.root
+    while (current?.left !== null) {
+      current = current!.left
+    }
+    return current.value
+  }
+  getMax() {
+    let current = this.root
+    while (current?.right !== null) {
+      current = current!.right
+    }
+    return current.value
+  }
+
+  find(value: T) {
+    let current = this.root
+    while (current) {
+      if (value === current.value) return current
+      current = value < current.value ? current.left : current.right
+    }
+    return null
+  }
+
+  contains(value: T) {
+    let current = this.root
+    while (current) {
+      if (value === current.value) return true
+      current = value < current.value ? current.left : current.right
+    }
+    return false
+  }
+
+  remove(value: T) {
+    if (!this.root) {
+      return 'Tree is empty'
+    } else {
+      return this.searchAndRemove(this.root, value)
+    }
+  }
+
+  findMin(root: Node<T>): Node<T> {
+    return !root.left ? root : this.findMin(root.left)
+  }
+
+  findMax(root: Node<T>): Node<T> {
+    return !root.right ? root : this.findMax(root.right)
+  }
+
+  searchAndRemove(node: Node<T> | null, value: T) {
+    if (!node) return null
+
+    if (value < node.value) {
+      node.left = this.searchAndRemove(node.left, value)
+      return node
+    } else if (value > node.value) {
+      node.right = this.searchAndRemove(node.right, value)
+      return node
+    } else {
+      // value === node.value
+      // No children
+      if (!node.left && !node.right) {
+        node = null
+        return node
+      }
+      // No left children
+      if (!node.left) {
+        node = node.right
+        return node
+      }
+      // No right children
+      if (!node.right) {
+        node = node.left
+        return node
+      }
+      // Node has two children
+      if (node.right) {
+        // Find min node on the right
+        let minNode = this.findMin(node.right)
+        // Replace node value with min node value
+        node.value = minNode.value
+        // Remove the min node
+        node.right = this.searchAndRemove(node.right, minNode.value)
+      }
+
+      return node
+    }
+  }
+}
+
+function contain() {
+  console.log('contain')
+  const bst = new BST<number>()
+  bst.add(2)
+  bst.add(5)
+  console.log(bst.contains(3))
+  bst.add(3)
+  console.log(bst.contains(3))
+}
+
+function find() {
+  console.log('find')
+  const bst = new BST<number>()
+  bst.add(3)
+  console.log(bst.find(4))
+  console.log(bst.find(3))
+}
+
+function getMin() {
+  console.log('getMin')
+  const bst = new BST<number>()
+  bst.add(2)
+  bst.add(5)
+  console.log(bst.getMin())
+}
+
+function getMax() {
+  console.log('getMax')
+  const bst = new BST<number>()
+  bst.add(2)
+  bst.add(5)
+  console.log(bst.getMax())
+}
+
+function remove() {
+  console.log('remove')
+  const bst = new BST<number>()
+  bst.add(2)
+  bst.add(1)
+  bst.add(5)
+  bst.add(3)
+  bst.remove(5)
+  console.log(bst.contains(5))
+}
+
+export const bst = {
+  fn: () => {
+    contain()
+    find()
+    getMin()
+    getMax()
+    remove()
+  },
+}
